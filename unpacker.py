4958fb4714e944475928628fe622230c9df0fbf6
Romet Vinnal <73593087+rometv@users.noreply.github.com>
2025-05-15 17:28:29 +0300

    Delete unpacker.py

--- unpacker.py
+++
@@ -1,8 +0,0 @@
-from zipfile import ZipFile
-
-from data_classes import Data
-from parser import parse
-
-def unpack(path: str) -> Data:
-    with ZipFile(path, 'r') as moodle_file:
-        return parse(moodle_file)
